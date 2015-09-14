097a70cc2349ec66f1a009954f2139a6a849dddb
Nam Vu Hoang <nam.vuhoang@aalto.fi>
2015-09-14 19:05:49 +0300

    Updated maven*.bat files

---
+++ maven-install-single.bat
@@ -0,0 +1,18 @@
+@ECHO.
+@ECHO *****************************************
+@ECHO BUILDING %1 (started)
+@ECHO *****************************************
+@ECHO.
+
+@CALL mvn clean install -f %1\pom.xml
+
+@ECHO.
+@ECHO *****************************************
+@ECHO BUILDING %1 (completed)
+@ECHO *****************************************
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+
