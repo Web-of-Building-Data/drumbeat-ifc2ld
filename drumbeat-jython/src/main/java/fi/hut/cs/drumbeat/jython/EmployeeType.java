649c766b295e74481b83781409fdf745e7775654
Nam Vu Hoang <nam.vuhoang@aalto.fi>
2015-12-08 16:52:32 +0200

    Removed unneccessary projects

--- drumbeat-jython/src/main/java/fi/hut/cs/drumbeat/jython/EmployeeType.java
+++
@@ -1,9 +0,0 @@
-package fi.hut.cs.drumbeat.jython;
-
-public interface EmployeeType {
-    
-    public String getEmployeeFirst();
-    public String getEmployeeLast();
-    public String getEmployeeId();
-    
-}
